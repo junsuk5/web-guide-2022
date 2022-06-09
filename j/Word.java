8792699d8825ba15001f122854c3534f80994873
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-06-09 11:11:11 +0900

    java 모음

---
+++ j/Word.java
@@ -0,0 +1,21 @@
+package com.survivalcoding;
+
+public class Word {
+    public static void main(String[] args) {
+        Word word = new Word("Hello World");
+        System.out.println(word.isVowel(0)); // false
+        System.out.println(word.isVowel(1)); // true
+    }
+
+    private String letters;
+
+    public Word(String letters) {
+        this.letters = letters.toLowerCase();
+    }
+
+    // i 번째 글자가 모음인지 // a, i, u, e, o
+    public boolean isVowel(int i) {
+        return letters.substring(i, i + 1).equals("a");
+    }
+
+}
